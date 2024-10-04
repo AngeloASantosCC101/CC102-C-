08f4957ad4aa2115450629a261c8243fedc58e52
Angelo A. Santos <santos.angelo.arazas@gmail.com>
2024-10-04 08:55:24 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string School, Department, CYL, YourFN, YourPA;
+	cout << "School : ";
+	cin >> School;
+	cout << "Department : ";
+	cin >> Department;
+	cout << "Course & Year Level : ";
+	cin >> CYL;
+	cout << "Your Full Name : ";
+	cin >> YourFN;
+	cout << "Your Personal Address : ";
+	cin >> YourPA;
+	
+	return 0;
+}
