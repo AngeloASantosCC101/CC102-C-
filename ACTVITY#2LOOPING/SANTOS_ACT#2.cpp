34ff3241607b903b5eeaa1b846de686b2d1ea201
Angelo A. Santos <santos.angelo.arazas@gmail.com>
2024-11-30 16:12:02 +0800

    Add files via upload

--- ACTVITY#2LOOPING/SANTOS_ACT#2.cpp
+++ ACTVITY#2LOOPING/SANTOS_ACT#2.cpp
@@ -5,9 +5,9 @@ int main() {
     char symbol1, symbol2, symbol3;
     cout << "Enter 1st symbol to use (*, ^, #, $, @): ";
     cin >> symbol1;
-    cout << "Enter 2nd symbol to use (*, ^, #, $, @): ";
+    cout << "Enter 2nd symbol to use (*, ^, $, @): ";
     cin >> symbol2;
-    cout << "Enter 3rd symbol to use (*, ^, #, $, @): ";
+    cout << "Enter 3rd symbol to use (*, $, @): ";
     cin >> symbol3;
 
     cout << "\nYou picked: " << symbol1 << " " << symbol2 << " " << symbol3 << "\n";
